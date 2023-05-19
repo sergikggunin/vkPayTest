a399117a8a42c1e2902517cb401b92f279147000
Segka test <sergeygunin@e1.ru>
2023-05-19 19:56:17 +0200

    Limit and new Test

--- src/main/kotlin/ru/netology/CalculateCommission.kt
+++ src/main/kotlin/ru/netology/CalculateCommission.kt
@@ -5,9 +5,24 @@ fun calculateCommission(
     previousTransfers: Int = 0,
     currentTransfer: Int
 ): Double {
+    // Проверка лимитов
+    when (cardType) {
+        "MasterCard", "Maestro" -> {
+            if (previousTransfers + currentTransfer > 150_000_00 || previousTransfers + currentTransfer > 600_000_00) {
+                return -1.0
+            }
+        }
+        "VK Pay" -> {
+            // Проверка ежемесячного и единоразового лимитов
+            if (currentTransfer > 15_000_00 || previousTransfers + currentTransfer > 40_000_00) {
+
+                return -1.0
+            }
+        }
+    }
     return when (cardType) {
         "MasterCard", "Maestro" -> {
-            if (previousTransfers + currentTransfer <= 75000) {
+            if (previousTransfers + currentTransfer <= 75_000_00) {
                 0.0
             } else {
                 0.006 * currentTransfer + 20
--- src/test/kotlin/CalculateCommissionKtTest.kt
+++ src/test/kotlin/CalculateCommissionKtTest.kt
@@ -11,7 +11,7 @@ class CalculateCommissionTest {
             previousTransfers = 1000_00,
             currentTransfer = 1000_00
         )
-        assertEquals(620.0, commission, 0.01)
+        assertEquals(0.0, commission, 0.01)
     }
 
     @Test
@@ -42,7 +42,7 @@ class CalculateCommissionTest {
             previousTransfers = 70_000_00,
             currentTransfer = 1_000_00
         )
-        assertEquals(620.0, commission, 0.01)
+        assertEquals(0.0, commission, 0.01)
     }
 
 
@@ -79,7 +79,7 @@ class CalculateCommissionTest {
             previousTransfers = 40000_00,
             currentTransfer = 16000_00
         )
-        assertEquals(0.0, commission, 0.01)
+        assertEquals(-1.0, commission, 0.01)
     }
     @Test
     fun testCalculateCommissionUnknownCardType2() {
@@ -130,7 +130,7 @@ class CalculateCommissionTest {
             cardType = "Visa",
             currentTransfer = 10_000_00
         )
-        assertEquals(70.0, commission, 0.01)
+        assertEquals(7500.0, commission, 0.01)
     }
 
     @Test
@@ -140,8 +140,48 @@ class CalculateCommissionTest {
             previousTransfers = 70_000_00,
             currentTransfer = 1_000_00
         )
-        assertEquals(620.0, commission, 0.01)
+        assertEquals(0.0, commission, 0.01)
     }
+    @Test
+    fun testDailyLimitMasterCard() {
+        val result = calculateCommission(
+            cardType = "MasterCard",
+            previousTransfers = 259_000_00,
+            currentTransfer = 1_000_00
+        )
+        assertEquals(-1.0, result, 0.01)
+    }
+    @Test
+    fun testMonthlyLimitMasterCard() {
+        val result = calculateCommission(
+            cardType = "MasterCard",
+            previousTransfers = 600_000_00,
+            currentTransfer = 1_000_00
+        )
+        // Здесь нужно указать ожидаемое значение, как и в предыдущем тесте
+        assertEquals(-1.0, result, 0.01)
+    }
+    @Test
+    fun testTransferLimitVKPay() {
+        val result = calculateCommission(
+            cardType = "VK Pay",
+            previousTransfers = 15_000_00,
+            currentTransfer = 1_000_00
+        )
+        // Здесь нужно указать ожидаемое значение, как и в предыдущих тестах
+        assertEquals(0.0, result, 0.01)
+    }
+    @Test
+    fun testMonthlyLimitVKPay() {
+        val result = calculateCommission(
+            cardType = "VK Pay",
+            previousTransfers = 40_000_00,
+            currentTransfer = 1_000_00
+        )
+        // Здесь нужно указать ожидаемое значение, как и в предыдущих тестах
+        assertEquals(-1.0, result, 0.01)
+    }
+
 }
 
 
